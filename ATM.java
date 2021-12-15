1c5b9f3d5c180c4c73f71818dfb2fcba0dddc8ac
dayren86 <90610568+dayren86@users.noreply.github.com>
2021-12-15 15:25:13 +0200

    1

---
+++ ATM.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static class ATM{
+        public int countBanknotes(int sum){
+            int result = 0;
+            int[] banknotes = {500,200,100,50,20,10,5,2,1};
+            int index = 0;
+            int count = banknotes.length;
+            while(count > 0){
+                 result += sum / banknotes[index];
+                 sum = sum % banknotes[index];
+                index++;
+                count--;
+            }
+            return result;
+        }
+    }
+}
